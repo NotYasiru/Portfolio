c82224572249bb14bf0f7426ab352dd9aa1c622b
Yasiru <92869313+Yasiru100@users.noreply.github.com>
2022-03-27 12:38:56 +0530

    update v1.2.7

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Programmer", "Designer", "3d Modeler","Video Edior", "YouTuber"],
+        strings: ["Web Developer", "Programmer", "Graphic Designer", "Game Dev","Video Edior"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Programmer", "Designer",  "3d Modeler","Video Edior", "YouTuber"],
+        strings: ["Web Developer", "Programmer", "Graphic Designer", "Game Dev","Video Edior"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
