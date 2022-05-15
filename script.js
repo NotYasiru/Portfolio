b11d8767d4d92d7de3b852b869d22dfaca4dec77
YasiruCoding <yhasanthas@gmail.com>
2022-05-15 14:30:25 +0530

    remove game dev

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Programmer", "Graphic Designer", "Game Dev","Video Editor"],
+        strings: ["Web Developer", "Programmer", "Graphic Designer" ,"Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Programmer", "Graphic Designer", "Game Dev","Video Editor"],
+        strings: ["Web Developer", "Programmer", "Graphic Designer" ,"Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
