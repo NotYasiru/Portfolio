9797c8188f23d209b38a212e3e63b89a7eb13592
NotYasiru <yhasanthas@gmail.com>
2023-02-08 11:43:02 +0530

    2023 update

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Programmer", "Graphic Designer" ,"Video Editor"],
+        strings: ["Web Developing", "Graphic Designing", "Programming", "Game Dev"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Programmer", "Graphic Designer" ,"Video Editor"],
+        strings: ["Web Developing", "Graphic Designing", "Programming", "Game Dev"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
